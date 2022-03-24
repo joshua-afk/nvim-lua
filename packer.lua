82f361882491691c92858cf6b75586bd1d6bae9c
Joshua Ryan V. Velasquez <joshuaryan.velasquez.0322@gmail.com>
2022-03-24 12:54:13 +0800

    feat: migrate plugins to packer

--- packer.lua
+++ packer.lua
@@ -1,5 +1,47 @@
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
 
-	-- add more plugins here
+	-- Core
+	use 'sunjon/shade.nvim'
+	use 'zegervdv/nrpattern.nvim'
+	use 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+
+	-- Navigation
+	use 'mg979/vim-visual-multi', {'branch': 'master'}
+	use 'psliwka/vim-smoothie'
+	use 'breuckelen/vim-resize'
+	use 'unblevable/quick-scope'
+	use 'airblade/vim-rooter'
+	use 'kyazdani42/nvim-tree.lua'
+	use 'nvim-telescope/telescope.nvim'
+
+	-- LSP, Completions & Snippets
+	use 'nvim-lua/plenary.nvim'
+
+	-- Linters & Fixers
+	use 'digitaltoad/vim-pug'
+
+	-- Themes
+	-- use 'morhetz/gruvbox'
+	use 'sainnhe/everforest'
+
+	-- Layout
+	use 'vim-airline/vim-airline'
+	use 'vim-airline/vim-airline-themes'
+	use 'mhinz/vim-startify'
+	use 'kyazdani42/nvim-web-devicons'
+
+	-- Git
+	-- use 'mhinz/vim-signify'
+	use 'tpope/vim-fugitive'
+
+	-- Etc
+	use 'tommcdo/vim-lion'
+	use 'tpope/vim-surround'
+	use 'tpope/vim-commentary'
+	use 'tpope/vim-repeat'
+	use 'tpope/vim-eunuch'
+
+	use 'junegunn/fzf', { 'do': { -> fzf#install() } }
+	use 'junegunn/fzf.vim'
 end)
